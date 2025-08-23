f36ab246cd3760b1bbad6cad5e793cc8163db2eb
Maicolalvarez-debug <maicolalvarez123.z@gmail.com>
2025-08-23 13:01:35 -0500

    jki

--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import React from 'react'
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-gray-950 text-white">
-      <div className="max-w-6xl mx-auto">{children}</div>
-    </div>
-  )
-}
