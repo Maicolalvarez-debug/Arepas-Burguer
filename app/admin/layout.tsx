528ea4c2986b9ad4523318086da1797ae0355e8f
Maicolalvarez-debug <maicolalvarez123.z@gmail.com>
2025-08-22 23:49:55 -0500

    Add files via upload

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-950 text-white">
